aa09f12731c76b0b85963f414b2dbb670a77a7a1
Arun_U <arunu2002@gmail.com>
2022-10-16 16:35:00 +0530

    With Copy constructor disabling functionality

--- main.cpp
+++ main.cpp
@@ -14,10 +14,21 @@ void clear() {
     }
 };
 struct Point_Safe{
-
+    Point_Safe(){}
+    //Point_Safe& operator = (Point_Safe& temp)=delete;
+    //Point_Safe(const Point_Safe& temp)= delete;
+    int x,y;
+    int* array;
+    void allocate(int size) {
+        array = new int(size);
+        }
+    void clear() {
+        delete array;
+        }
 };
 int main()
 {
+    //-*** Risky code************
     Point_Risky p;
     p.x=1;
     p.y=2;
@@ -34,6 +45,26 @@ int main()
     std::cout<<p1.array<<'\n';
     std::cout<<p1.array[0]<<'\n'; //Accessing Deleted Arrays
     std::cout<<p1.array[0]<<'\n'; //Accessing Deleted Arrays
+    //-*********Safe code*******
+    Point_Safe ps;
+    ps.x=1;
+    ps.y=2;
+    ps.allocate(10);
+    ps.array[0]=10;
+    ps.array[1]=20;
+    std::cout<<ps.array<<'\n';
+    std::cout<<ps.array[0]<<'\n';
+    std::cout<<ps.array[0]<<'\n';
+    ps.clear();
+
+    Point_Safe ps1 = ps; //Impossible - If ( Point_Safe(const Point_Safe& temp)= delete; )
+    Point_Safe ps2;
+     ps2.x=1;
+     ps2.y=2;
+
+     ps2 = ps1;//Impossible - If ( Point_Safe& operator = (Point_Safe& temp)=delete; )
+
+
     return 0;
 }
 
